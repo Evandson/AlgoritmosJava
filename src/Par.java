aa9d0b71de39c5d3c5e4bb57f22216ee709d74d8
Evandson Maranhão <evandson.cm@gmail.com>
2016-04-11 03:35:13 -0300

    Adição da variável de incremento para alocar elemento em posição vazia do vetor.

--- src/Par.java
+++ src/Par.java
@@ -4,48 +4,49 @@ import java.util.Scanner;
  * Created by Evandson Maranhão on 09/04/2016.
  */
 public class Par {
-   public static void main(String args[]) {
-       int qtdPartidas = 0;
-       String nomeJogador1;
-       String nomeJogador2;
-       int maoJogador1;
-       int maoJogador2;
-       int teste = 1;
-       String array[] = new String[1000];
-       int aux = 0;
+    public static void main(String args[]) {
+        int qtdPartidas = 0;
+        String nomeJogador1;
+        String nomeJogador2;
+        int maoJogador1;
+        int maoJogador2;
+        int teste = 1;
+        String array[] = new String[1000];
+        int aux = 0;
+        int cont = 0;
 
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-       while (teste != 0) {
-               System.out.println("Quantidade de Partidas:");
-               qtdPartidas = sc.nextInt();
-           if (qtdPartidas == 0){
-               break;
-           }
-               aux += qtdPartidas;
+        while (teste != 0) {
+            System.out.println("Quantidade de Partidas:");
+            qtdPartidas = sc.nextInt();
+            if (qtdPartidas == 0) {
+                break;
+            }
+            aux += qtdPartidas;
 
-               System.out.println("Nome do Jogador1:");
-               nomeJogador1 = sc.next();
-               System.out.println("Nome do Jogador2:");
-               nomeJogador2 = sc.next();
-           for (int i = 0; i < qtdPartidas; i++) {
-               {
-                   System.out.println("Mão do Jogador1:");
-                   maoJogador1 = sc.nextInt();
-                   System.out.println("Mão do Jogador2:");
-                   maoJogador2 = sc.nextInt();
+            System.out.println("Nome do Jogador1:");
+            nomeJogador1 = sc.next();
+            System.out.println("Nome do Jogador2:");
+            nomeJogador2 = sc.next();
+            for (int i = 0; i < qtdPartidas; i++) {
 
-                   if ((maoJogador1 + maoJogador2)% 2 == 0){
-                        array[i] = nomeJogador1;
-                   } else {
-                        array[i] = nomeJogador2;
-                   }
-               }
-           }
-       }
+                System.out.println("Mão do Jogador1:");
+                maoJogador1 = sc.nextInt();
+                System.out.println("Mão do Jogador2:");
+                maoJogador2 = sc.nextInt();
 
-       for (int i = 0; i < aux; i++){
-               System.out.println(array[i]);
-           }
-       }
-   }
+                if ((maoJogador1 + maoJogador2) % 2 == 0) {
+                    array[cont] = (nomeJogador1);
+                } else {
+                    array[cont] = nomeJogador2;
+                }
+                cont+=1;
+            }
+        }
+
+        for (int i = 0; i < aux; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
